9f791e25f173a4ab2a557717ea37bd7e4b99b6e8
iwasMateo <iwasMateo@gmail.com>
2026-02-26 12:10:33 +0100

    fix paths

--- buttonscript.js
+++ buttonscript.js
@@ -5,13 +5,13 @@ let playbuttonimg = playbutton.querySelector("img");
 var audio = document.getElementById("audio");
 function switchPlay() {
     if (!audio.paused){
-        playbuttonimg.src = "/images/play.png";
+        playbuttonimg.src = "images/play.png";
         audio.pause()
     } else if (audio.paused) {
-        playbuttonimg.src="/images/pause.png";
+        playbuttonimg.src="images/pause.png";
         audio.play();
     } else {
-        playbuttonimg.src = "/images/play.png";
+        playbuttonimg.src = "images/play.png";
         audio.pause();
     }
 }
