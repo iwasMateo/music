bf82d8569ef1a437f0049b1ecdd958973f38456f
iwasMateo <iwasMateo@gmail.com>
2026-02-26 11:54:07 +0100

    Correct play button functionality
    Fix play button logic to check audio state correctly.

--- buttonscript.js
+++ buttonscript.js
@@ -4,10 +4,10 @@ let backbutton = document.getElementById("backbutton");
 let playbuttonimg = playbutton.querySelector("img");
 var audio = document.getElementById("audio");
 function switchPlay() {
-    if (playbuttonimg.src.includes("images/pause.png")){
+    if (!audio.paused){
         playbuttonimg.src = "/images/play.png";
         audio.pause()
-    } else if (playbuttonimg.src.includes("images/play.png")) {
+    } else if (audio.paused)) {
         playbuttonimg.src="/images/pause.png";
         audio.play();
     } else {
